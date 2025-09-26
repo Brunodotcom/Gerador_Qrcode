998027b145f3cca2213bfce4392bf558fc238cb5
Bruno Rodrigues <bruno.usa1995@hotmail.com>
2025-09-26 17:55:18 -0300

    versao inicial

---
+++ app.py
@@ -0,0 +1,26 @@
+import qrcode
+from PIL import Image
+from flask import Flask, render_template, request
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        link = request.form.get('INPUT')
+        if not link:
+            return render_template('index.html', enviado=False, erro='Por favor, insira um link válido.')
+
+
+        filename = f'qr_{abs(hash(link))}.png'
+        path = os.path.join('static', filename)
+
+        img = qrcode.make(link)
+        img.save(path)
+
+        return render_template('index.html', enviado=True, img=filename)
+    return render_template('index.html', enviado=False)
+
+app.run(debug=True)
